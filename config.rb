9bba48459aea5dd2451777729e74b0fbd08b4f27
Kris Steigerwald <kris@calltrackingmetrics.com>
2015-05-16 19:54:05 -0400

    - blog set up

--- config.rb
+++ config.rb
@@ -36,10 +36,12 @@
 # activate :automatic_image_sizes
 
 # Reload the browser automatically whenever files change
-# configure :development do
-#   activate :livereload
-# end
+configure :development do
+   activate :livereload
+end
+activate :blog do |blog|
 
+end
 # Methods defined in the helpers block are available in templates
 # helpers do
 #   def some_helper
